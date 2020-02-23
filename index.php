68753b966ae8772b7776c9ab033428476bdc98f9
mrbrunelli <matheus.brunelli@gmail.com>
2020-02-23 03:52:50 -0300

    recebendo valores do input no javascript

--- index.php
+++ index.php
@@ -1,47 +1,64 @@
-<?php
-include './includes/header.php';
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
 
-<style>
-    .login {
-        position: absolute;
-        top: 20%;
-        left: 30%;
-    }
-</style>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login CRUD JS</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
 
-<div class="card mb-3 shadow login" style="max-width: 540px;">
-    <div class="row no-gutters">
-        <div class="col-md-4 align-self-center">
-            <img src="./app/storage/js.png" class="card-img" alt="...">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">CRUD JS</h5>
-                <form>
+
+
+</head>
+
+<body>
+
+    <style>
+        .login {
+            position: absolute;
+            top: 20%;
+            left: 30%;
+        }
+    </style>
+
+    <div class="card mb-3 shadow login" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col-md-4 align-self-center">
+                <img src="./app/storage/js.png" class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">CRUD JS</h5>
                     <div class="row">
                         <div class="col-md-12 form-group">
                             <label for="login"><small>Login</small></label>
-                            <input type="text" class="form-control" name="login" required placeholder="Digite seu login">
+                            <input type="text" class="form-control" name="login" id="login" placeholder="Digite seu login">
                         </div>
                         <div class="col-md-12 form-group">
                             <label for="senha"><small>Senha</small></label>
-                            <input type="password" class="form-control" name="senha" required placeholder="Digite sua senha">
+                            <input type="password" class="form-control" name="senha" id="senha" placeholder="Digite sua senha">
                         </div>
                         <div class="ml-3 col-md-12 form-group form-check">
                             <input type="checkbox" class="form-check-input">
                             <label for="check" class="form-check-label"><small>Lembrar-me</small></label>
                         </div>
-                        <div class="col-md-12 form-group">
-                            <button class="btn btn-primary btn-block">Entrar</button>
+                        <div class="col-md-12">
+                            <button type="button" class="btn btn-primary btn-block" onclick="login()">Entrar</button>
                         </div>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<?php
-include './includes/footer.php';
-?>
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+    <script src="./login.js" async></script>
+
+</body>
+
+</html>
---
+++ login.js
@@ -0,0 +1,6 @@
+async function login() {
+    let login = document.querySelector('#login').value
+    let senha = document.querySelector('#senha').value
+
+    alert(`${login} ${senha}`)
+}
