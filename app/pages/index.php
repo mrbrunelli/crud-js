30fe385e38038056eba800194afc00926aaef72f
mrbrunelli <matheus.brunelli@gmail.com>
2020-02-23 20:46:41 -0300

    criando navbar no index

--- app/pages/index.php
+++ app/pages/index.php
@@ -1,3 +1,21 @@
 <?php
 session_start();
-echo "ola " . $_SESSION['UsuarioNome'];
+
+if (!isset($_SESSION['UsuarioID'])) {
+    header('location: ../../index.php');
+}
+include '../../includes/header.php';
+?>
+
+<nav class="navbar sticky-top navbar-dark bg-dark">
+    <div class="container">
+        <a href="#" class="nav-brand">CRUD JS</a>
+        <ul class="nav">
+            <li class="nav-item">Sair</li>
+        </ul>
+    </div>
+</nav>
+
+<?php
+include '../../includes/footer.php';
+?>
